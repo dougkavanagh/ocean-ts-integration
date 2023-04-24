902545f2e1319e5e137ca35448a75ea8d10cba2b
dougkavanagh <dougkavanagh@gmail.com>
2023-04-24 07:45:36 -0400

    fixed env bug

--- projects/smart-client/src/env.ts
+++ projects/smart-client/src/env.ts
@@ -5,7 +5,7 @@ export const PORT = parsed?.PORT ?? "8888";
 export const TEST_LAUNCH_CONTEXT = parsed?.TEST_LAUNCH_CONTEXT ?? "";
 export const FHIR_SERVER_URL =
   parsed?.FHIR_SERVER_URL ?? "https://your.smart.server";
-export const CLIENT_ID = parsed?.CC_CLIENT_ID ?? "";
+export const CLIENT_ID = parsed?.CC_CLCLIENT_IDIENT_ID ?? "";
 export const CLIENT_SECRET = parsed?.CLIENT_SECRET;
 export const TEST_SITE_NUM = parsed?.TEST_SITE_NUM ?? "123456";
 
