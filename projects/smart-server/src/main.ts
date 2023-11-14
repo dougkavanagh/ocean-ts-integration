45e33d869fd1e41bdc7b1f27ad4bcc0566ecba8c
dougkavanagh <dougkavanagh@gmail.com>
2023-11-14 13:28:05 -0500

    Clarified launch instructions

--- projects/smart-server/src/main.ts
+++ projects/smart-server/src/main.ts
@@ -6,7 +6,13 @@ import express, {
   Response,
   Router,
 } from "express";
-import { FHIR_ENDPOINT_PREFIX, PORT } from "./env";
+import {
+  ALLOWED_CLIENT_ID,
+  AUTH_ENDPOINT_PREFIX,
+  FHIR_ENDPOINT_PREFIX,
+  PORT,
+  OCEAN_ACTION
+} from "./env";
 import { verifyBearerToken } from "./auth-utils";
 import { isErrorMessage } from "./core";
 import { handleTokenRequest } from "./handlers/token-handler";
@@ -18,28 +24,8 @@ import { handleAuthorizeRequest } from "./handlers/authorize-handler";
 import logger from "./logger";
 import { handleKeysRequest } from "./handlers/keys-handler";
 
-const app: express.Application = express();
-const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-};
-app.use(errorHandler);
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  bodyParser.json({ type: ["application/fhir+json", "application/json"] })
-);
-app.use(
-  cors({
-    origin: true,
-    credentials: true,
-  })
-);
-createRouters();
-app.use(function (req, res, next) {
-  logger.warn(`404 not found: ${req.method} ${req.path}`);
-  res.status(404).send("Sorry, page not found");
-});
-export default app;
+const AUTHORIZE_PATH = "/authorize";
+const TOKEN_PATH = "/token";
 
 function createRouters() {
   createAuthRouter();
@@ -81,19 +67,40 @@ function createAuthorizedFhirRouter() {
 
 function createAuthRouter() {
   const authRouter = express.Router();
-  authRouter.all("/authorize", handleAuthorizeRequest);
-  authRouter.post("/token", handleTokenRequest);
+  authRouter.all(AUTHORIZE_PATH, handleAuthorizeRequest);
+  authRouter.post(TOKEN_PATH, handleTokenRequest);
   authRouter.get("/keys", handleKeysRequest);
-  app.use("/auth", authRouter);
+  WellKnownOidcConfigurationHandler.setup(authRouter);
+  app.use(AUTH_ENDPOINT_PREFIX, authRouter);
 }
 
 function createFhirRoutes(fhirRouter: Router) {
   PatientFhirHandler.setup(fhirRouter);
   PatientFhirHandler.setup(fhirRouter);
-  WellKnownOidcConfigurationHandler.setup(fhirRouter);
   WellKnownSmartConfigurationHandler.setup(fhirRouter);
 }
 
+const app: express.Application = express();
+const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+};
+app.use(errorHandler);
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  bodyParser.json({ type: ["application/fhir+json", "application/json"] })
+);
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+createRouters();
+app.use(function (req, res, next) {
+  logger.warn(`404 not found: ${req.method} ${req.path}`);
+  res.status(404).send("Sorry, page not found");
+});
 app.listen(PORT, () => {
   const serverUrl = `http://localhost:${PORT}`;
   console.log(`SMART server is listening at ${serverUrl}`);
@@ -102,11 +109,28 @@ app.listen(PORT, () => {
     `${serverUrl}${FHIR_ENDPOINT_PREFIX}/.well-known/smart-configuration`
   );
   console.log(
-    `${serverUrl}${FHIR_ENDPOINT_PREFIX}/.well-known/openid-configuration`
+    `${serverUrl}${AUTH_ENDPOINT_PREFIX}/.well-known/openid-configuration`
   );
-  console.log("Simulating a 'View Patient in Ocean' launch button...");
+  console.log();
+  console.log(
+    `Ocean should have an allowlist entry with the following parameters:`
+  );
+  console.log(`Client ID: ` + ALLOWED_CLIENT_ID);
+  console.log(`Client Secret: *** (check your environment variable)`);
+  console.log(`Server URL: ${serverUrl}${FHIR_ENDPOINT_PREFIX}`);
+  console.log(
+    `Token Endpoint: ${serverUrl}${AUTH_ENDPOINT_PREFIX}${TOKEN_PATH}`
+  );
+  console.log(
+    `Authorization Endpoint: ${serverUrl}${AUTH_ENDPOINT_PREFIX}${AUTHORIZE_PATH}`
+  );
+  console.log(`Token Issuer URL: ${serverUrl}${AUTH_ENDPOINT_PREFIX}`);
+  console.log();
+
+  console.log(
+    "Imagine this is an EMR with a 'View Patient in Ocean' launch button..."
+  );
   const OCEAN_TEST_SITE_NUM = "1234";
-  const OCEAN_ACTION = "viewMap";
   const smartLaunchUrl = createSmartLaunchUrl({
     context: {
       user: {
@@ -122,6 +146,7 @@ app.listen(PORT, () => {
     action: OCEAN_ACTION,
   });
   console.log(
-    `The EMR opens the following SMART launch URL in a new tab in the user's default web browser:\n${smartLaunchUrl}`
+    `Clicking this button in EMR opens the following SMART launch URL in a new tab in the user's default web browser:\n${smartLaunchUrl}`
   );
 });
+export default app;
