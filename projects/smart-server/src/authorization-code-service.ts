b1b42135fabd4da578f0c0963016159a55de9ea0
dougkavanagh <dougkavanagh@gmail.com>
2023-11-14 13:27:50 -0500

    removed comment

--- projects/smart-server/src/authorization-code-service.ts
+++ projects/smart-server/src/authorization-code-service.ts
@@ -44,7 +44,7 @@ async function createFromLaunchToken(
     encounter,
     siteId,
     expiresAt: expiresAt(),
-    idTokenIssuer: iss,//
+    idTokenIssuer: iss,
     codeChallenge: codeChallenge,
     codeChallengeMethod: codeChallengeMethod,
     scopes: scopes,
